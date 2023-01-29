33a262166e014cd720a49597a459ffa9543fdd9c
stella-mary <mariastellamary@outlook.com>
2023-01-29 19:38:23 +0530

    Quotation data changed

--- src/Pages/QuotationPage.js
+++ src/Pages/QuotationPage.js
@@ -98,73 +98,13 @@ const QuotationPages = () => {
 
     const handleSubmit = (e) => {
         // e.preventDefault()
-        axios.post('http://localhost:7005/quotation', {
-
-            quotationPartName: quotationInfo.quotationPartName,
-            quotationQuantity: quotationInfo.quotationQuantity,
-            quotationfValue: quotationInfo.quotationfValue,
-            quotationRemark: quotationInfo.quotationRemark,
-            quotationEmail: quotationInfo.quotationEmail,
-            quotationQuotation: quotationInfo.quotationQuotation,
-            quotationMovingType: quotationInfo.quotationMovingType,
-            quotationCompanyParty: quotationInfo.quotationCompanyParty,
-            quotationPartyName: quotationInfo.quotationPartyName,
-            quotationPhone: quotationInfo.quotationPhone,
-            quotationDate: quotationInfo.quotationDate,
-            quotationPackingDate: quotationInfo.quotationPackingDate,
-            quotationDeliveryDate: quotationInfo.quotationDeliveryDate,
-            quotationCountry: quotationInfo.quotationCountry,
-            quotationStates: quotationInfo.quotationStates,
-            quotationCity: quotationInfo.quotationCity,
-            quotationPincode: quotationInfo.quotationPincode,
-            quotationAddress: quotationInfo.quotationAddress,
-            quotationFloor: quotationInfo.quotationFloor,
-            quotationLift: quotationInfo.quotationLift,
-            quotationfCountry: quotationInfo.quotationfCountry,
-            quotationfStates: quotationInfo.quotationfStates,
-            quotationfCity: quotationInfo.quotationfCity,
-            quotationfPincode: quotationInfo.quotationfPincode,
-            quotationfAddress: quotationInfo.quotationfAddress,
-            quotationfFloor: quotationInfo.quotationfFloor,
-            quotationfLift: quotationInfo.quotationfLift,
-            quotationfreightCharge: quotationInfo.quotationfreightCharge,
-            quotationAdvancePaid: quotationInfo.quotationAdvancePaid,
-            quotationPackingCharge: quotationInfo.quotationPackingCharge,
-            quotationunPackingCharge: quotationInfo.quotationunPackingCharge,
-            quotationLoadingCharge: quotationInfo.quotationLoadingCharge,
-            quotationunLoadingCharge: quotationInfo.quotationunLoadingCharge,
-            quotationPackingMaterialCharge: quotationInfo.quotationPackingMaterialCharge,
-            quotationStorageCharge: quotationInfo.quotationStorageCharge,
-            quotationCarTPT: quotationInfo.quotationCarTPT,
-            quotationMiscellCharge: quotationInfo.quotationMiscellCharge,
-            quotationOtherCharge: quotationInfo.quotationOtherCharge,
-            quotationstCharge: quotationInfo.quotationstCharge,
-            quotationGreenTax: quotationInfo.quotationGreenTax,
-            quotationSurCharge: quotationInfo.quotationSurCharge,
-            quotationGSTQuote: quotationInfo.quotationGSTQuote,
-            quotationGST: quotationInfo.quotationGST,
-            quotationGSTType: quotationInfo.quotationGSTType,
-            quotationRemark: quotationInfo.quotationRemark,
-            quotationDiscount: quotationInfo.quotationDiscount,
-            quotationInsuranceType: quotationInfo.quotationInsuranceType,
-            quotationInsuranceCharge: quotationInfo.quotationInsuranceCharge,
-            quotationfGST: quotationInfo.quotationfGST,
-            quotationDeclaration: quotationInfo.quotationDeclaration,
-            quotationfInsuranceType: quotationInfo.quotationfInsuranceType,
-            quotationfInsuranceCharge: quotationInfo.quotationfInsuranceCharge,
-            quotationffGst: quotationInfo.quotationffGst,
-            quotationfDeclaration: quotationInfo.quotationfDeclaration,
-            quotationLoad: quotationInfo.quotationLoad,
-            quotationDown: quotationInfo.quotationDown,
-            quotationfLoad: quotationInfo.quotationfLoad,
-            quotationNeed: quotationInfo.quotationNeed,
-
-        }).then((response) => {
-            console.log(response)
-            navigate("/SaveQuotation")
-            console.log(response.data)
-            alert("quotation form submitted")
-        })
+        axios.post('http://localhost:7005/quotation', quotationInfo)
+            .then((response) => {
+                console.log(response)
+                console.log(response.data)
+                alert("quotation form submitted")
+                navigate("/SaveQuotation")
+            })
 
         setQuotationInfo("")
     }
